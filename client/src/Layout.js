3552f1f1667169e06928d7cbd9308acd81790e7c
Joydeep8822154667 <meghadas184@gmail.com>
2023-08-20 19:56:11 +0530

    first commit

---
+++ client/src/Layout.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import {Outlet} from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <main>
+      {/* <div>Hello</div> */}
+      <Header />
+      
+    </main>
+  );
+}
